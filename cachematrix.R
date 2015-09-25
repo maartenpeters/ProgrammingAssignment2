2ec989e70844348f3fb61e2bfa92b45b81693452
maartenpeters <maarten.peters@gmail.com>
2015-09-25 10:39:26 +0200

    Completed function
    Used the example and converted it to invert a matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,38 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  ## Gets the solution from earlier calculation
+  m <- x$getsolve()
+  ## Checks if the solution exists
+  if(!is.null(m)) {
+    ## If solution exists, pass message and return solution
+    message("getting cached data")
+    return(m)
+  }
+  ## (Else) get the matrix from the makeCacheMatrix function
+  data <- x$get()
+  ## Invert the matrix and store in m
+  m <- solve(data, ...)
+  ## Pass m to makeCacheMatrix function to store solution in cache
+  x$setsolve(m)
+  ## Return m
+  m
 }
